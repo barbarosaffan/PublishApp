864a27221f39755e7ef389114a51da0019f25b5e
barbarosaffan <barbarosaffan@gmail.com>
2020-12-19 17:06:22 +0300

    Rewrote everything and made the app working

--- App.js
+++ App.js
@@ -1,77 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState, Component } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#A9A9A9',
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center'
   },
-  viewBorders: {
-    borderColor: 'black',
-    borderWidth: 1,
-  },
-  textInputStyle: {
+  input: {
+    width: '70%',
     borderColor: 'black',
     borderWidth: 1,
     padding: 10,
-    height: 50,
-    width: 50,
   },
-  topView: {
-    padding: 50,
-    flexDirection: 'row',
-    height: 400,
-    justifyContent: 'space-around',
-    alignItems: 'stretch',
+  itemList: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
   },
-  textInputView: {
-    backgroundColor: '#8A8F83',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  viewAndButtonPicker: {
-    backgroundColor: 'purple',
-    flex: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  pickerStyle : {
-    height: 50,
-    width: 50,
-  }
 });
 
 const App = () => {
-  const [selectedValue, setSelectedValue] = useState("java");
-  const [value, onChangeText] = React.useState()
+  const [enteredBlog, setEnteredBlog] = useState('');
+  const [publishments, setPublishments] = useState([]);
+
+  const BlogInputHandler = (enteredText) => {
+    setEnteredBlog(enteredText);
+  };
+  const addBlogHandler = () => {
+    setPublishments(currentBlogs => [
+      ...currentBlogs, 
+      { key: Math.random().toString(), value: enteredBlog }
+    ]);
+  };
+
   return (
-    <View id = 'MainView'>
-     <View style = {styles.topView}>
-        <View style = {styles.textInputView}>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
         <TextInput
-          style = {styles.textInputStyle}
-          onChangeText = {text => onChangeText(text)}
-          value = {value}
-          placeholder = "Buraya yazınız."
+          placeholder="Buraya yazınız"
+          style={styles.input}
+          onChangeText={BlogInputHandler}
+          value={enteredBlog}
         />
-        </View>
-        <View style = {styles.viewAndButtonPicker}>
-        <Picker
-          style = {styles.pickerStyle}
-          selectedValue={selectedValue}
-          onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}>
-          <Picker.Item label="Java" value="java" />
-          <Picker.Item label="JavaScript" value="js" />
-        </Picker>
-        </View>
+        <Button title="GÖNDER" onPress={addBlogHandler} />
       </View>
-    <StatusBar style="auto" />
-  </View>
+      <FlatList 
+        data={publishments} 
+        renderItem={itemData=> (
+          <View style={styles.itemList}>
+            <Text>{itemData.item.value}</Text>
+          </View>  
+        )}
+      />  
+    </View> 
   );
 }
 
